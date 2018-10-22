e3be768b98cd84dc8c765dbc63d821dbe03af3f8
ohmez <kkid_92@hotmail.com>
2018-10-22 14:02:13 -0600

    flower of life animated

--- assets/javascript/canvas.js
+++ assets/javascript/canvas.js
@@ -12,10 +12,28 @@ function Circle(x, y, xVelocity, yVelocity, radius) {
     this.radius = radius;
     
     this.draw = function() {
-        console.log()
+        c.beginPath(); // start of flower of life
+        c.arc(this.x,this.y, this.radius, 0, Math.PI *2,false); // first circle
+        c.strokeStyle = "purple";
+        c.stroke();
         c.beginPath();
-        c.arc(this.x,this.y, this.radius, 0, Math.PI *2,false);
-        c.strokeStyle = "rgb(138,43,226)";
+        c.arc(this.x,this.y+30,30,0,Math.PI *2,false); // first circle below
+        c.strokeStyle = "purple";
+        c.stroke();
+        c.beginPath();
+        c.arc(this.x+26.25,this.y+15, 30, 0, Math.PI * 2, false); // first circle to the right
+        c.stroke();
+        c.beginPath();
+        c.arc(this.x-26.25,this.y+15, 30, 0, Math.PI * 2, false); // first circle to the left
+        c.stroke();
+        c.beginPath();
+        c.arc(this.x+26.25, this.y+45, 30, 0, Math.PI * 2, false); // 2nd right circle down
+        c.stroke();
+        c.beginPath();
+        c.arc(this.x-26.25, this.y+45, 30, 0, Math.PI * 2, false); // 2nd left circle down
+        c.stroke();
+        c.beginPath();
+        c.arc(this.x,this.y+60,30,0,Math.PI *2,false); // center bottom circle
         c.stroke();
     };
     this.update = function () {
@@ -34,14 +52,14 @@ function Circle(x, y, xVelocity, yVelocity, radius) {
 
 
 var circleArr = [];
-for (var i = 0; i <100; i++) {    
-    var radius = 40;
+for (var i = 0; i <10; i++) {    
+    var radius = 30;
     var x = Math.random() * (innerWidth - radius *2) + radius; // starting x position.
     var y = Math.random() * (innerHeight - radius *2) + radius;
     var xVelocity = (Math.random() - 0.5) *3; // moving at 3 pixels 
     var yVelocity = (Math.random() - 0.5) *3; 
         circleArr.push(new Circle(x,y,xVelocity,yVelocity,radius));
-        var circle = new Circle(200,200, 3,3,40);
+        var circle = new Circle(200,200, 3,3,30);
 
 }
 function animate() {
